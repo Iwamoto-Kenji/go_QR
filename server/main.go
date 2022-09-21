3b5ec3bddfc9f8e6632956e5462e155c19993a30
kenji.iwamoto <>
2022-09-21 15:40:42 +0900

    画面上からQRコードを生成可能にする

--- server/main.go
+++ server/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"strconv"
+	"image/png"
+	
+	"go-qr-app/qrgen"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -13,8 +18,54 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/generate", generateQRCode)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
 	}
+}
+
+func generateQRCode(w http.ResponseWriter, r *http.Request) {
+	param := r.URL.Query()
+
+	url := param.Get("url")
+	if url == "" {
+		http.Error(w, "missing url", http.StatusBadRequest)
+		return
+	}
+
+	widthStr := param.Get("width")
+	width, err := strconv.Atoi(widthStr)
+	if err != nil {
+		http.Error(w, "invalid width", http.StatusBadRequest)
+		return
+	}
+
+	heightStr := param.Get("height")
+	height, err := strconv.Atoi(heightStr)
+	if err != nil {
+		http.Error(w, "invalid height", http.StatusBadRequest)
+		return
+	}
+
+	// validation
+	if width <= 45 {
+		http.Error(w, "width must be more than 45px", http.StatusBadRequest)
+		return
+	}
+	if height <= 45 {
+		http.Error(w, "height must be more than 45px", http.StatusBadRequest)
+		return
+	}
+
+	img, err := qrgen.GenQRCode(url, width, height)
+	if err != nil {
+		errStr := fmt.Sprintf("failed to generate QR code %v", err)
+		http.Error(w, errStr, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	png.Encode(w, img)
 }
